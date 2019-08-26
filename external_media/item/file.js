88ff24aeab7efbb1a70499ecee8f35a323e1603a
Aurelien David <aurelien.david@telecom-paristech.fr>
2019-08-26 14:18:01 +0200

    add external_media to lfs

---
+++ external_media/item/file.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:80c74d33a3042aae668ae46e22cb9140c85cc74886791b6e2fce3d26a228559f
+size 7046
---
+++ external_media/m2ts/TNT_MUX_R1_586MHz_10s.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2610b16a23dada9ccc938dfa4c34aad63dd87c30ec3dc6d623c11c7a0ef77411
+size 28200000
